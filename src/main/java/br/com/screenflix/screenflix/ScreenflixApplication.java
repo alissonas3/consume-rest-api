472cf3d31b0592314b9ad267365465683511fabf
Alisson Alves <alisson.asilva3@gmail.com>
2025-05-13 21:37:21 -0300

    feat: main menu and refactor main class

--- src/main/java/br/com/screenflix/screenflix/ScreenflixApplication.java
+++ src/main/java/br/com/screenflix/screenflix/ScreenflixApplication.java
@@ -1,18 +1,9 @@
 package br.com.screenflix.screenflix;
 
-import br.com.screenflix.screenflix.model.EpisodeInfo;
-import br.com.screenflix.screenflix.model.SeasonInfo;
-import br.com.screenflix.screenflix.model.SerieInfo;
-import br.com.screenflix.screenflix.service.ConvertData;
-import br.com.screenflix.screenflix.service.RequestsAPI;
+import br.com.screenflix.screenflix.menu.mainMenu;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.sound.midi.Soundbank;
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenflixApplication implements CommandLineRunner {
@@ -24,32 +15,8 @@ public class ScreenflixApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		var request = new RequestsAPI();
-		var responseData = request.getData("https://www.omdbapi.com/?t=the+last+of+us&apikey=849866d8");
-		System.out.println(responseData);
-
-		ConvertData convert = new ConvertData();
-
-		SerieInfo serie = convert.getData(responseData, SerieInfo.class);
-		System.out.println("Some details from serie: " + serie);
-
-		System.out.println("------------------");
-
-		responseData = request.getData("https://omdbapi.com/?t=the+last+of+us&season=1&episode=2&apikey=849866d8");
-		EpisodeInfo episode = convert.getData(responseData, EpisodeInfo.class);
-		System.out.println("Some details from a specific episode: " + episode);
-
-		System.out.println("------------------");
-
-		List<SeasonInfo> seasons = new ArrayList<>();
-
-		for(int i = 1; i <= serie.seasons(); i++) {
-			responseData = request.getData("https://www.omdbapi.com/?t=the+last+of+us&season=" + i + "&apikey=849866d8");
-			SeasonInfo season = convert.getData(responseData, SeasonInfo.class);
-			seasons.add(season);
-		}
-		seasons.forEach(System.out::println);
-
+		mainMenu menu = new mainMenu();
+		menu.loadMainMenu();
 
 	}
 }
--- src/main/java/br/com/screenflix/screenflix/menu/mainMenu.java
+++ src/main/java/br/com/screenflix/screenflix/menu/mainMenu.java
@@ -1,15 +1,50 @@
 package br.com.screenflix.screenflix.menu;
+import br.com.screenflix.screenflix.model.EpisodeInfo;
+import br.com.screenflix.screenflix.model.SeasonInfo;
+import br.com.screenflix.screenflix.model.SerieInfo;
+import br.com.screenflix.screenflix.service.ConvertData;
+import br.com.screenflix.screenflix.service.RequestsAPI;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class mainMenu {
 
     private Scanner reader = new Scanner(System.in);
+    private final String URL = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=849866d8";
 
     public void loadMainMenu() {
 
-//        System.out.println("Search your favorite serie: ");
-//        var serie = reader.nextLine();
+        System.out.println("Search your favorite serie: ");
+        var serie = reader.nextLine();
 
+        var request = new RequestsAPI();
+        var responseData = request.getData(URL + serie.replace(" ", "+") + API_KEY);
+        System.out.println(responseData);
+
+        ConvertData convert = new ConvertData();
+
+        SerieInfo serieDetail = convert.getData(responseData, SerieInfo.class);
+        System.out.println("Some details from serie: " + serieDetail);
+
+		System.out.println("------------------");
+
+		responseData = request.getData("https://omdbapi.com/?t=the+last+of+us&season=1&episode=2&apikey=849866d8");
+		EpisodeInfo episode = convert.getData(responseData, EpisodeInfo.class);
+		System.out.println("Some details from a specific episode: " + episode);
+
+		System.out.println("------------------");
+
+		List<SeasonInfo> seasons = new ArrayList<>();
+
+		for(int i = 1; i < serieDetail.seasons(); i++) {
+			responseData = request.getData("https://www.omdbapi.com/?t=the+last+of+us&season=" + i + "&apikey=849866d8");
+			SeasonInfo season = convert.getData(responseData, SeasonInfo.class);
+			seasons.add(season);
+		}
+		seasons.forEach(System.out::println);
 
     }
 
