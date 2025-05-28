0914995d09d4119b39ea2400a72c02223acc9dd5
Alisson Alves Silva <alissil@futura.sbo>
2025-05-28 17:58:13 -0300

    feat: filters with steams()

--- src/main/java/br/com/screenflix/screenflix/menu/mainMenu.java
+++ src/main/java/br/com/screenflix/screenflix/menu/mainMenu.java
@@ -6,8 +6,10 @@ import br.com.screenflix.screenflix.service.ConvertData;
 import br.com.screenflix.screenflix.service.RequestsAPI;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class mainMenu {
 
@@ -25,28 +27,36 @@ public class mainMenu {
         System.out.println(responseData);
 
         ConvertData convert = new ConvertData();
-
         SerieInfo serieDetail = convert.getData(responseData, SerieInfo.class);
         System.out.println("Some details from serie: " + serieDetail);
-
 		System.out.println("------------------");
 
 //		responseData = request.getData("https://omdbapi.com/?t=the+last+of+us&season=1&episode=2&apikey=849866d8");
 //		EpisodeInfo episode = convert.getData(responseData, EpisodeInfo.class);
 //		System.out.println("Some details from a specific episode: " + episode);
-//
-//		System.out.println("------------------");
 
 		List<SeasonInfo> seasons = new ArrayList<>();
-
 		for(int i = 1; i <= serieDetail.seasons(); i++) {
 			responseData = request.getData(URL + serie.replace(" ", "+") + "&season=" + i + API_KEY);
 			SeasonInfo season = convert.getData(responseData, SeasonInfo.class);
 			seasons.add(season);
 		}
 		seasons.forEach(System.out::println);
-
         seasons.forEach(t -> t.episodes().forEach(e -> System.out.println(e.title())));
+
+
+        System.out.println("TOP 5 EPISODES!");
+        List<EpisodeInfo> episodeInfo = seasons.stream()
+                .flatMap(t -> t.episodes().stream())
+                .collect(Collectors.toList());
+
+        episodeInfo.stream()
+//                .filter(e -> !e.number().equals("N/A"))
+                        .sorted(Comparator.comparing(EpisodeInfo::number).reversed())
+                        .limit(5)
+                .forEach(System.out::println);
+
+//        episodeInfo.forEach(System.out::println);
 
     }
 
